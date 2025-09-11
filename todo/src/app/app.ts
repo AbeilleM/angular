80eda93e77b0b9217968f51fb15db811d3bc49e3
Maia <maia.lo@wanadoo.fr>
2025-09-12 01:23:26 +0200

    Simple liste et initialisation du service TodoService

--- todo/src/app/app.ts
+++ todo/src/app/app.ts
@@ -4,9 +4,16 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  // templateUrl: './app.html',
+  template: `
+    <ul>
+    @for (todo of listExample; track todo.id) {
+      <li>{{ todo.task }}</li>
+    }
+    </ul>
+  `,    
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todo');
+  listExample = [{id: 0, task : 'Mettre à jour le CSS'}, {id: 1, task : 'Faire une vraie interface TODO'}, {id: 2, task : 'Créer le service TodoService'}];
 }
